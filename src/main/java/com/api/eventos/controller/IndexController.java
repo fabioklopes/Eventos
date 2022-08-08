2969ac9ed3c8825365ab4b397cccd4982eefda56
Fábio Klevinskas Lopes <fabioklopes@live.com>
2022-08-08 14:50:21 -0300

    Lançamento da primeira versão dos arquivos

---
+++ src/main/java/com/api/eventos/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.api.eventos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("Chamou o método index()");
+        return "home";
+    }
+    
+}
